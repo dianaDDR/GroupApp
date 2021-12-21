cd90cf26b3d5aeae149346f8af6603c9d79f6bca
mconamor <marciatorres12.1@gmail.com>
2021-12-21 20:01:52 +0100

    -m

--- app/src/main/java/com/example/groupapp/MainActivity.kt
+++ app/src/main/java/com/example/groupapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.groupapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun commitButton(view: android.view.View) {
+        commitActivity()
+    }
+    private fun commitActivity() {
+        val IntComm = Intent(this, Activitycommit::class.java)
+        startActivity(IntComm)
     }
 }
