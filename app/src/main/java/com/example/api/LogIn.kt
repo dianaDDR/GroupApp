f47d0ad05edb002fb45210dc67a1a5cfe544a5b7
thcito <jplatalmeira@gmail.com>
2022-02-08 21:03:21 +0100

    Splah

--- app/src/main/java/com/example/api/LogIn.kt
+++ app/src/main/java/com/example/api/LogIn.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 
 class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
+        setTheme(R.style.Theme_API)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+
     }
 }
