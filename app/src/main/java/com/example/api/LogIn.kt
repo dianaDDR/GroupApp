abe7932f77f5166bb59a33923a64097ab43abca6
VANT <nymehatake@gmail.com>
2022-02-09 18:29:04 +0100

    iconos

--- app/src/main/java/com/example/api/LogIn.kt
+++ app/src/main/java/com/example/api/LogIn.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(1000)
+        Thread.sleep(3000)
         setTheme(R.style.Theme_API)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
