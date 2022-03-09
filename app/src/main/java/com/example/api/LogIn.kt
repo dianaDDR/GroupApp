5ba0e67769f4aa6df464c399cbbdafca99fe549b
Jose Plata Almeira <Thcitoo@gmail.com>
2022-03-09 11:17:15 +0100

    Registro y login finalizado.

--- app/src/main/java/com/example/api/LogIn.kt
+++ app/src/main/java/com/example/api/LogIn.kt
@@ -1,37 +1,150 @@
 package com.example.api
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.api.databinding.ActivityLogInBinding
 import com.example.api.databinding.ActivityMapsBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignIn.*
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LogIn : AppCompatActivity() {
+    private val GOOGLE_SIGN_CODE=100
 
-        //Inflamos el XML
-    private lateinit var binding: ActivityLogInBinding
+    //Inflamos el XML
+   //private lateinit var binding: ActivityLogInBinding
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(2000)
         setTheme(R.style.Theme_API)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
+        // Para ver los loggin en la consola de Firebase
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("Mensaje", "Firebase tutorial")
+        analytics.logEvent("InitScreen", bundle)
+        google()
+        comprobarSesion()
 
-    binding = ActivityLogInBinding.inflate(layoutInflater)
+        // función para comprobar si ya hay un usuario logueado
 
-        //Botón de los 2 inputLayout (Email y Contraseña)
+        //google
+        val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        val googleClient = getClient(this, googleConf)
+
+
+        val registerButton : ImageButton = findViewById(R.id.botonGoogleLogin);
+        registerButton.setOnClickListener{
+            googleClient.signOut()
+            val intent = googleClient.getSignInIntent()
+            startActivityForResult(intent, 100)
+        }
+    }
+    /*binding = ActivityLogInBinding.inflate(layoutInflater)
+
+        // inputLayout (Email y Contraseña)
         binding.botonInicioSesion.setOnClickListener{
-            val email = binding.textFieldEmail.editText?.text.toString()
-            val contrasenia = binding.textFieldContrasena.editText?.text.toString()
+            val email = binding.email.editText?.text.toString()
+            val contrasena = binding.contrasena.editText?.text.toString()
 
             //El Toast nos muestra el email y la contraseña introducidas en un mensaje en el footer.
-            Toast.makeText(this, "Email->$email , contrasena-> $contrasenia",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Email->$email , contrasena-> $contrasena",Toast.LENGTH_LONG).show()
         }
-        setContentView(binding.root)
+        setContentView(binding.root)*/
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_CODE) {
+            val task = getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful,
+                //authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                if(account != null){
+                    val credential = GoogleAuthProvider.
+                    getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().
+                    signInWithCredential(credential)
+                        .addOnCompleteListener{
+                            if(it.isSuccessful){
+                                // Navegamos a la siguiente
+                                irPantallaMenu()
+                            }
+                            else{
+                                 //Error
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                showError()
+
+            }
+        }
+    }
+    fun google(){
+        val googleConf = GoogleSignInOptions.
+        Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+        requestIdToken(getString(R.string.default_web_client_id))
+            .
+            requestEmail().
+            build()
+        val gClient = getClient(this,
+            googleConf)
+        val googleButton : ImageButton = findViewById(R.id.botonGoogleLogin);
+        googleButton.setOnClickListener{
+            gClient.signOut()
+            val intent = gClient.getSignInIntent()
+            startActivityForResult(intent, 100)
+        }
+
+    }
+
+    private fun comprobarSesion(){
+        val preferences = getSharedPreferences(getString(R.string.preferences), MODE_PRIVATE)
+        // RECOGEMOS EL EMAIL
+        val email = preferences.getString("email", null)
+        if(email != null){
+            irPantallaMenu()
+        }
+    }
+
+    private fun showError() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error con Firebase")
+        builder.setPositiveButton( "Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showErrorContraseña() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("El email o la contraseña introducida no es correcta")
+        builder.setPositiveButton( "Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
 
     }
 
@@ -42,4 +155,29 @@ class LogIn : AppCompatActivity() {
     fun onClickIrRegistro(view: View) {
         irPantallaRegistro()
     }
+    private fun irPantallaMenu() {
+        val menuIntent = Intent(this, Menu::class.java)
+        startActivity(menuIntent)
+    }
+
+
+    fun onClickInicioSesion(view: View) {
+        // Obtenemos los campos de texto y los guardamos en objetos
+        val email: TextInputEditText = findViewById(R.id.email);
+        val passw: TextInputEditText = findViewById(R.id.contrasena);
+        // Comprobamos que los campos no son nulos
+        var emailString = email.text.toString()
+        var passwordString = passw.text.toString()
+        if (emailString != null && passwordString != null) {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        irPantallaMenu()
+                    } else {
+                        showErrorContraseña()
+                    }
+                }
+        }
+    }
+
 }
--- app/src/main/java/com/example/api/Registro.kt
+++ app/src/main/java/com/example/api/Registro.kt
@@ -1,8 +1,15 @@
 package com.example.api
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +17,79 @@ class Registro : AppCompatActivity() {
         setContentView(R.layout.activity_registro)
     }
 
+    fun setBottom(){
+        val Nombre : TextInputEditText = findViewById(R.id.textFieldNombre);
+        val Apellido : TextInputEditText = findViewById(R.id.textFieldApellido);
+        val Email : TextInputEditText = findViewById(R.id.textFieldEmail);
+        val Passwd : TextInputEditText = findViewById(R.id.textFieldContrasena);
+        val Passwd2 : TextInputEditText = findViewById(R.id.textFieldConfirmaContrasena);
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("Usuarios").document(Email.text.toString()).set(hashMapOf(
+            "Nombre" to Nombre.text.toString(),
+            "Apellido" to Apellido.text.toString(),
+            "Passwd" to Passwd.text.toString(),
+            "Passwd2" to Passwd2.text.toString()
+        ))
+    }
+
     fun onclickAtrasRegistro(view: View) {
         onBackPressed()
     }
+
+    private fun showError() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error con firebase")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    fun irPantallaMenu() {
+        var menuIntent = Intent(this, Menu::class.java)
+        startActivity(menuIntent)
+    }
+
+    fun onClickConfirmar(view:View) {
+
+        val email: TextInputEditText = findViewById(R.id.textFieldEmail);
+        val cont: TextInputEditText = findViewById(R.id.textFieldContrasena);
+        val cont2: TextInputEditText = findViewById(R.id.textFieldConfirmaContrasena)
+
+        if (email != null && cont != null && cont2!=null) {
+            if (email.text.toString() != "" && cont.text.toString() != "") {
+                var emailString = email.text.toString()
+                var contString = cont.text.toString()
+                var cont2String = cont2.text.toString()
+
+                if ( contString == cont2String) {
+
+                    FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(emailString, contString)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                setBottom()
+                                irPantallaMenu()
+
+                            } else {
+                                showError()
+                            }
+                        }
+
+                }else{
+                    showError()
+                }
+            }
+        } else {
+
+        }
+        //Se realizara cuando se presione el boton NEXT
+
+        //Intent es una clase que recibe 2 parametros, donde estamos y a donde vamos
+
+        //Creamos un objeto de la clase Intent
+
+    }
+
 }
