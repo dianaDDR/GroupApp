a7ca13f160aed803f324c9784c434a653bbee85c
Alex <lealalejandro319@gmail.com>
2022-03-19 19:26:39 +0100

    Recycl

---
+++ app/src/main/java/com/example/api/Cuenta.kt
@@ -0,0 +1,34 @@
+package com.example.api
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class Cuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_cuenta)
+
+        val recycler : RecyclerView= findViewById(R.id.fotos)
+        val adapter : RecyclerViewAdapter = RecyclerViewAdapter()
+        //Configuración Adapter
+        adapter.RecyclerViewAdapter(contacts(), this)
+
+        //Configuración RecyclerView
+        recycler.hasFixedSize()
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
+    }
+
+    private fun contacts(): MutableList<Modelo> {
+
+        var contactos : MutableList<Modelo> = ArrayList()
+        contactos.add(Modelo("Ale" , R.drawable.fondo2))
+        contactos.add(Modelo("Ale" , R.drawable.fondo2 ))
+        contactos.add(Modelo("Ale" , R.drawable.fondo2))
+        contactos.add(Modelo("Ale" , R.drawable.fondo2 ))
+
+        return contactos
+    }
+}
